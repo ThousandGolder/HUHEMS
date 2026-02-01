85571520edd12f08cfee20c6c04f40e19733262d
ThousandGolder <kumaworku22@gmail.com>
2026-02-01 22:56:25 +0300

    adding time show and hide

---
+++ HEMS/Migrations/20260201190443_AddBannedColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HEMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBannedColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Banned",
+                table: "StudentExams",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Banned",
+                table: "StudentExams");
+        }
+    }
+}
